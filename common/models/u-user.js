9ed2d13ee03c86b59da7bc88991d8ab2b68fa0cb
pranav kumar <pranavkumar@pranavs-MacBook-Air.local>
2018-12-16 17:53:10 +0530

    next commit

--- common/models/u-user.js
+++ common/models/u-user.js
@@ -1,5 +1,35 @@
 'use strict';
-
-module.exports = function(Uuser) {
-
+module.exports = function(UUser) {
+    UUser.addContacts = async (id, data) => {
+        try {
+            let uUser = await UUser.findById(id);
+            if (!uUser) {
+                throw new Error(`UUserId ${id} does not exist.`);
+            }
+            let res = await Promise.all(data.map((contact) => uUser.contacts.create(contact)));
+            return res;
+        } catch (err) {
+            throw err;
+        }
+    }
+    UUser.remoteMethod('addContacts', {
+        accepts: [{
+            arg: 'id',
+            type: 'string'
+        }, {
+            arg: 'contacts',
+            type: 'array',
+            http: {
+                source: "body"
+            }
+        }],
+        returns: {
+            arg: 'result',
+            type: 'object'
+        },
+        http: {
+            verb: 'post',
+            path: '/:id/addContacts'
+        }
+    });
 };
