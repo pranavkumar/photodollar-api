89ca5dfea7fa9f4db0de2535c6b81c830587e6b6
pranav kumar <pranavkumar@pranavs-MacBook-Air.local>
2018-12-22 14:38:55 +0530

    next commit

--- common/models/u-user.js
+++ common/models/u-user.js
@@ -29,7 +29,7 @@ module.exports = function(UUser) {
         },
         http: {
             verb: 'post',
-            path: '/:id/addContacts'
+            path: '/:id/contacts/multi'
         }
     });
 };
--- server/server.js
+++ server/server.js
@@ -1,29 +1,29 @@
 'use strict';
-
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-
+var express = require('express');
 var app = module.exports = loopback();
-
+var path = require("path");
+var fileapp = express();
+fileapp.use('/files/responseImages', express.static(path.resolve(__dirname, '../storage/responseImages')));
+// app.use(loopback.static('/images', path.resolve(__dirname, '../storage/responseImages')));
 app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
+    fileapp.listen(5500);
+    // start the web server
+    return app.listen(function() {
+        app.emit('started');
+        var baseUrl = app.get('url').replace(/\/$/, '');
+        console.log('Web server listening at: %s', baseUrl);
+        if (app.get('loopback-component-explorer')) {
+            var explorerPath = app.get('loopback-component-explorer').mountPath;
+            console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+        }
+    });
 };
-
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
-  if (err) throw err;
-
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+    if (err) throw err;
+    // start the server if `$ node server.js`
+    if (require.main === module) app.start();
 });
