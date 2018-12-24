4f4511a5c2dff36e2769432f42d6da9c556531a0
pranav kumar <pranavkumar@pranavs-MacBook-Air.local>
2018-12-24 13:08:01 +0530

    next commit

--- common/models/u-request.js
+++ common/models/u-request.js
@@ -1,5 +1,77 @@
 'use strict';
-
-module.exports = function(Urequest) {
-
+const _ = require("lodash");
+var app = require("../../server/server");
+module.exports = function(URequest) {
+    URequest.getExpectators = async (id) => {
+        try {
+            let uRequest = await URequest.findById(id);
+            if (!uRequest) {
+                throw new Error(`No request with id ${id}`);
+            }
+            let expectatorIds = uRequest.expectatorIds || [];
+            return expectatorIds;
+        } catch (err) {
+            throw err
+        }
+    }
+    URequest.addExpectator = async (id, expectator) => {
+    	// console.log(expectator);
+        try {
+            let uRequest = await URequest.findById(id);
+            if (!uRequest) {
+                throw new Error(`No request with id ${id}`);
+                return;
+            }
+            let uUser = await app.models.UUser.findById(expectator.id);
+            // console.log(uUser);
+            if (!uUser) {
+                throw new Error(`No user with id ${id}`);
+                return;
+            }
+            uRequest.expectatorIds.push(expectator.id);
+            uRequest.expectatorIds = _.uniq(uRequest.expectatorIds);
+            // console.log(uRequest.expectatorIds);
+            // uRequest.expectatorIds = expectatorIds;
+            await uRequest.save();
+            return true;
+        } catch (err) {
+            throw err
+        }
+    }
+    URequest.remoteMethod('getExpectators', {
+        accepts: [{
+            arg: 'id',
+            type: 'string'
+        }],
+        returns: {
+            arg: 'result',
+            type: 'array',
+            root: true
+        },
+        http: {
+            verb: 'get',
+            path: '/:id/expectators'
+        }
+    });
+    URequest.remoteMethod('addExpectator', {
+        accepts: [{
+            arg: 'id',
+            type: 'string'
+        }, {
+            arg: "expectator",
+            type: "object",
+            http: {
+                source: "body"
+            }
+        }],
+        returns: {
+            arg: 'result',
+            type: 'array',
+            root: true
+        },
+        http: {
+            verb: 'post',
+            path: '/:id/expectators'
+        }
+    });
 };
