7927c5a7ef67f421f913cbbcf8954543ea429862
pranav kumar <pranavkumar@pranavs-MacBook-Air.local>
2018-12-24 15:58:12 +0530

    next commit

--- common/models/u-request.js
+++ common/models/u-request.js
@@ -15,7 +15,6 @@ module.exports = function(URequest) {
         }
     }
     URequest.addExpectator = async (id, expectator) => {
-    	// console.log(expectator);
         try {
             let uRequest = await URequest.findById(id);
             if (!uRequest) {
@@ -23,17 +22,38 @@ module.exports = function(URequest) {
                 return;
             }
             let uUser = await app.models.UUser.findById(expectator.id);
-            // console.log(uUser);
             if (!uUser) {
-                throw new Error(`No user with id ${id}`);
+                throw new Error(`No user with id ${expectator.id}`);
                 return;
             }
-            uRequest.expectatorIds.push(expectator.id);
-            uRequest.expectatorIds = _.uniq(uRequest.expectatorIds);
-            // console.log(uRequest.expectatorIds);
-            // uRequest.expectatorIds = expectatorIds;
-            await uRequest.save();
-            return true;
+            let index = _.findIndex(uRequest.expectators, function(o) {
+                return o.id == expectator.id;
+            })
+            if (index < 0) {
+                let subtractPoints = expectator.points;
+                let finalPoints = uUser.points - subtractPoints;
+                if (finalPoints < 0) {
+                    throw new Error("Not enough points");
+                } else {
+                    uUser.points = finalPoints;
+                }
+                uRequest.expectators.push(expectator);
+                await uRequest.save();
+                await uUser.save();
+                return {
+                    isExpecting: true
+                };
+            } else {
+                let oldExpectator = uRequest.expectators[index];
+                let addPoints = oldExpectator.points;
+                uRequest.expectators.splice(index, 1);
+                uUser.points = uUser.points + addPoints;
+                await uRequest.save();
+                await uUser.save();
+                return {
+                    isExpecting: false
+                };
+            }
         } catch (err) {
             throw err
         }
--- common/models/u-user.js
+++ common/models/u-user.js
@@ -1,5 +1,34 @@
 'use strict';
+var app = require("../../server/server");
+var _ = require("lodash");
 module.exports = function(UUser) {
+    UUser.getFeed = async (id) => {
+        try {
+            let uUser = await UUser.findById(id);
+            if (!uUser) {
+                throw new Error(`UUserId ${id} does not exist.`);
+            }
+            let requests = await app.models.URequest.find({
+                include: ["UResponses", "UUser"]
+            });
+            requests = _.map(requests, (request) => {
+                let index = _.findIndex(request.expectators, function(o) {
+                    console.log(o);
+                    console.log(id);
+                    return o.id == id;
+                })
+                if (index >= 0) {
+                    request.isExpecting = true;
+                } else {
+                    request.isExpecting = false;
+                }
+                return request;
+            })
+            return requests;
+        } catch (err) {
+            throw err;
+        }
+    }
     UUser.addContacts = async (id, contacts) => {
         try {
             let uUser = await UUser.findById(id);
@@ -30,6 +59,21 @@ module.exports = function(UUser) {
         http: {
             verb: 'post',
             path: '/:id/contacts/multi'
+        }
+    });
+    UUser.remoteMethod('getFeed', {
+        accepts: [{
+            arg: 'id',
+            type: 'string'
+        }],
+        returns: {
+            arg: 'result',
+            type: 'array',
+            root: true
+        },
+        http: {
+            verb: 'get',
+            path: '/:id/feed'
         }
     });
 };
