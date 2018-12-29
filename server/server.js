adfa7ea60b5dc61066d8ec13afb0c96a69dc45b4
pranav kumar <pranavkumar@pranavs-MacBook-Air.local>
2018-12-29 17:25:20 +0530

    next commit

--- server/server.js
+++ server/server.js
@@ -7,10 +7,10 @@ var path = require("path");
 var fileapp = express();
 fileapp.use('/files/responseImages', express.static(path.resolve(__dirname, '../storage/responseImages')));
 fileapp.use('/files/userProfileImages', express.static(path.resolve(__dirname, '../storage/userProfileImages')));
-app.start = function() {
+app.start = function () {
     fileapp.listen(5500);
     // start the web server
-    return app.listen(function() {
+    return app.listen(function () {
         app.emit('started');
         var baseUrl = app.get('url').replace(/\/$/, '');
         console.log('Web server listening at: %s', baseUrl);
@@ -20,9 +20,14 @@ app.start = function() {
         }
     });
 };
+app.middleware('initial', '/api/*', function logResponse(req, res, next) {
+    // same code as sample # 2 
+    console.log("ooo some request");
+    next();
+});
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
+boot(app, __dirname, function (err) {
     if (err) throw err;
     // start the server if `$ node server.js`
     if (require.main === module) app.start();
