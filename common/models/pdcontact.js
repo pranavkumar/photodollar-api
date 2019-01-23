48b4f72a0394c095e53c47325e73db11da0ad8f2
pranav kumar <pranavkumar@pranavs-MacBook-Air.local>
2019-01-23 21:47:10 +0530

    next commit

--- common/models/pdcontact.js
+++ common/models/pdcontact.js
@@ -1,5 +1,37 @@
 'use strict';
 
 module.exports = function(Pdcontact) {
-
+    function normalizeMobile(mobile) {
+        
+        mobile = mobile.replace(/ /g, "").replace(/\+/g, "").replace(/\-/g, "");
+        if (mobile.length > 10) {
+            let diff = mobile.length - 10;
+            mobile = mobile.substring(diff);
+        }
+        if (mobile.length == 10) {
+            return Number(mobile);
+        } else {
+            return 0;
+        }
+    }
+    Pdcontact.observe("before save", (ctx, next) => {
+        
+        if (ctx.isNewInstance) {
+            if (ctx.instance.id) {
+                ctx.instance.remoteId = ctx.instance.id;
+                ctx.instance.id = undefined;
+            }
+        }
+        if (ctx.instance.phoneNumbers && _.isArray(ctx.instance.phoneNumbers) && ctx.instance.phoneNumbers.length > 0) {
+            let mobileObj = ctx.instance.phoneNumbers[0];
+            if (mobileObj.number) {
+                let normalizedMobile = normalizeMobile(mobileObj.number);
+                if (!_.isNaN(normalizedMobile)) {
+                    ctx.instance.normalizedMobile = normalizedMobile;
+                }
+            }
+        }
+        
+        next();
+    });
 };
