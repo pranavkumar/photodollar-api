21059eabafc25aee7d27aa25ec22cd66aa74493a
pranav kumar <pranavkumar@pranavs-MacBook-Air.local>
2019-01-24 17:32:27 +0530

    next commit

--- common/models/storage.js
+++ common/models/storage.js
@@ -3,24 +3,27 @@ var app = require("../../server/server");
 
 module.exports = function (Storage) {
 	Storage.afterRemote('upload', async function (ctx, res, next) {
-		// console.log(JSON.stringify(res.result));
 		let { result: { files: { file }, fields } } = res;
-		console.log(file);
-		console.log(fields);
 		if (file[0].container == 'responseImages') {
 			//gonna add a response
-			let { userId, requestId } = fields;
+			let { userId, requestId, caption } = fields;
 			userId = userId[0];
 			requestId = requestId[0];
+
+
 
 			console.log(userId);
 			console.log(requestId);
 			let pdrequest = await app.models.Pdrequest.findById(requestId);
-			let response  = await pdrequest.responses.create({ userId: userId, image: file[0] });
+			let responseObj = { userId: userId, image: file[0]};
+			if (caption) {
+				responseObj.caption = caption[0];
+			}
+			let response = await pdrequest.responses.create(responseObj);
 
 			res.result["response"] = response;
-			
+
 		}
-		
+
 	})
 };
