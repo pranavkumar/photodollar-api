00983f363e4a318cdda95056006712ed65ba9b7f
pranav kumar <pranavkumar@pranavs-MacBook-Air.local>
2019-01-24 21:30:18 +0530

    next commit

--- common/models/storage.js
+++ common/models/storage.js
@@ -6,18 +6,18 @@ module.exports = function (Storage) {
 		let { result: { files: { file }, fields } } = res;
 		if (file[0].container == 'responseImages') {
 			//gonna add a response
-			let { userId, requestId, caption } = fields;
+			let { userId, requestId, meta } = fields;
 			userId = userId[0];
 			requestId = requestId[0];
-
+			meta = JSON.parse(meta[0]);
 
 
 			console.log(userId);
 			console.log(requestId);
 			let pdrequest = await app.models.Pdrequest.findById(requestId);
-			let responseObj = { userId: userId, image: file[0]};
-			if (caption) {
-				responseObj.caption = caption[0];
+			let responseObj = { userId: userId, image: file[0] };
+			if (meta.caption) {
+				responseObj.caption = meta.caption;
 			}
 			let response = await pdrequest.responses.create(responseObj);
 
